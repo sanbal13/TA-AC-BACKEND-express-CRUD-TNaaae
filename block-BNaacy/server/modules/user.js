42ac90fc7ca8d30cf6c758f1535008ee995490f3
Santosh Balchandran <santoshbalchandran@gmail.com>
2021-06-08 00:33:33 +0530

    Explain with Code: Create Operation

---
+++ block-BNaacy/server/modules/user.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, lowercase: true, match: /@/ },
+    age: {type: Number, min: 18, max: 100},
+    username: {type: String, required: true} 
+});
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
