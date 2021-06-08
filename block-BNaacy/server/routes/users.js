c91dc47d0cce6aaba7c39a175c6617f7c7bdd9b4
Santosh Balchandran <santoshbalchandran@gmail.com>
2021-06-08 11:54:55 +0530

    Add timestamps to User Model

--- block-BNaacy/server/routes/users.js
+++ block-BNaacy/server/routes/users.js
@@ -5,9 +5,11 @@ let router = express.Router();
 router.get('/', (req, res) => {
     res.render('user');
 });
+
 router.get('/new', (req, res) => {
     res.render('userForm');
 });
+
 router.post('/', (req, res) => {
      console.log('req.body');
      User.create(req.body, (err, createdUser) => {
